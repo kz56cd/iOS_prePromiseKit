0e1361d916c2b9097751b6799a223cb0c848876f
kz56cd <kz56cd@gmail.com>
2018-10-11 16:28:15 +0900

    Init project

---
+++ PrePromiseKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrePromiseKit
+//
+//  Created by Masakazu Sano on 2018/10/11.
+//  Copyright © 2018年 kz56cd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PrePromiseKitTests/PrePromiseKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  PrePromiseKitTests.swift
+//  PrePromiseKitTests
+//
+//  Created by Masakazu Sano on 2018/10/11.
+//  Copyright © 2018年 kz56cd. All rights reserved.
+//
+
+import XCTest
+@testable import PrePromiseKit
+
+class PrePromiseKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
