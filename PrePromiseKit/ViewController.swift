24c7d17fbaa8eceb86b67223cdcc044b83114795
kz56cd <kz56cd@gmail.com>
2018-10-11 17:34:45 +0900

    Update promise func (try .when)

--- PrePromiseKit/ViewController.swift
+++ PrePromiseKit/ViewController.swift
@@ -32,32 +32,57 @@ extension ViewController {
 //        print("3")
         
         // NOTE: use .then (複数非同期処理を順番に実行)
-        _ = Promise<String> { seal in
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    // seal.fulfill("Done!! 01")
-                    seal.reject(MyError.unknownerror) // errorを発生させる
-                    print(1)
-                }
-            }.then { result -> Promise<String> in
-                Promise<String> { seal in
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                        print(2)
-                        seal.fulfill("Done!! 02")
+//        _ = Promise<String> { seal in
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+//                    // seal.fulfill("Done!! 01")
+//                    seal.reject(MyError.unknownerror) // errorを発生させる
+//                    print(1)
+//                }
+//            }.then { result -> Promise<String> in
+//                Promise<String> { seal in
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+//                        print(2)
+//                        seal.fulfill("Done!! 02")
+//                    }
+//                }
+//            }.then { result -> Promise<String> in
+//                Promise<String> { seal in
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+//                        print(3)
+//                        seal.fulfill("Done!! 03")
+//                    }
+//                }
+//            }.done { value in
+//                print(".done")
+//                print(value)
+//            }.catch { error in // 例外は .catch で定義できる
+//                print(error.localizedDescription)
+//            }
+        
+        // NOTE: when (複数処理を並列実行)
+        let promises = (0...3).map { i -> Promise<String> in
+            Promise<String> { seal in
+                DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                    if i == 2 {
+                        seal.reject(MyError.unknownerror)
+                    } else {
+                        seal.fulfill("DONE: \(i)")
                     }
+                    print(i)
                 }
-            }.then { result -> Promise<String> in
-                Promise<String> { seal in
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
-                        print(3)
-                        seal.fulfill("Done!! 03")
-                    }
-                }
-            }.done { value in
-                print(".done")
-                print(value)
-            }.catch { error in // 例外は .catch で定義できる
-                print(error.localizedDescription)
             }
+        }
+        
+        when(resolved: promises).done { values in
+            values.forEach {
+                switch $0 {
+                case .fulfilled(let value):
+                    print(value)
+                case .rejected(let error):
+                    print(error)
+                }
+            }
+        }
     }
 }
 
